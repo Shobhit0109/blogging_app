5a8cbb24b4ee3fada7b27c38a312657ba9135b17
Shobhit0109 <shobhit.ug21@nsut.ac.in>
2024-08-03 09:30:16 +0530

    Sat Aug  3 09:30:16 AM IST 2024

--- app/admin/component/Sidebar.tsx
+++ app/admin/component/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { Gauge, Layers2, LayoutList, User } from "lucide-react";
 import Link from "next/link";
+import React from "react";
 
 export default function Sidebar() {
   const link = [
@@ -27,8 +28,8 @@ export default function Sidebar() {
   return (
     <section className="h-screen w-[200px] border-r p-6">
       <ul className="flex w-full flex-col gap-6">
-        {link.map((item) => (
-          <Link href={item.link}>
+        {link.map((item, index) => (
+          <Link href={item.link} key={index}>
             <li className="flex items-center gap-3 rounded-full bg-blue-100 px-5 py-2 text-black">
               {item.icon} <span className="font-semibold">{item.name}</span>
             </li>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,6 @@
 // import { redirect } from "next/navigation";
 import Link from "next/link";
+import React from "react";
 
 export default function NotFound() {
   // redirect("/");
